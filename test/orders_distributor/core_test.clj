6d5bbb9644da4d7a5c949f9b7533d553181236a1
Izpa <ispius.prime@gmail.com>
2020-03-04 13:57:01 +0300

    init

---
+++ test/orders_distributor/core_test.clj
@@ -0,0 +1,7 @@
+(ns orders-distributor.core-test
+  (:require [clojure.test :refer :all]
+    [orders-distributor.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
