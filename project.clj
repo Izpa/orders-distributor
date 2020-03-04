abd9ad9a82c41f0d760018073fdfd70e9bc394ce
Izpa <ispius.prime@gmail.com>
2020-03-04 16:20:56 +0300

    [WIP]

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject orders-distributor "0.1.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [environ "1.1.0"]
                  [morse "0.2.4"]
