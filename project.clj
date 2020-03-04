a57f6a86922ce5f13b3949a11390e294a77435ab
Izpa <ispius.prime@gmail.com>
2020-03-04 16:15:05 +0300

    Fix project.clj

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [environ "1.1.0"]
                  [morse "0.2.4"]
-                 [compojure "1.6.1"]
+                 [compojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [ring/ring-json "0.4.0"]
                  [clj-http "3.9.1"]]
