9754abcfb8b5256d27fe26762792c9b9527a4548
Izpa <ispius.prime@gmail.com>
2020-03-06 07:22:29 +0300

    Refactoring

--- src/orders_distributor/orders/core.clj
+++ src/orders_distributor/orders/core.clj
@@ -4,18 +4,26 @@
             [orders-distributor.orders.db :as db]
             [orders-distributor.settings :as s]))
 
+(defn- remove-command [text]
+  (let [clear-text-vector (-> text
+                              (str/split #" ")
+                              (subvec 1))]
+    (str/join " " clear-text-vector)))
+
+(defn- user-string [first-name last-name user-name]
+  (str first_name " " last_name " (" username ")"))
+
 (defn new [msg]
   (let [chat-id (get-in msg [:telegram_chat :external_id])
         {{:keys [first_name last_name username]} :telegram_user} msg
         order-id (db/create-order! (:id msg))
         order-text (-> msg
                        :text
-                       (str/split #" ")
-                       (subvec 1))
-        order-to-distribute (->> order-text
-                                 (str/join " ")
-                                 (str "Новый заказ " order-id "\n"
-                                      "От " first_name " " last_name " (" username ")\n"))]
+                       remove-command)
+        from-string (user-string first_name last_name username)
+        order-to-distribute (str "Новый заказ " order-id "\n"
+                                 "От " from-string "\n"
+                                 order-text)]
     (api/send-text s/distributor-telegram-token
                    s/distributor-chat-telegram-id
                    order-to-distribute)
