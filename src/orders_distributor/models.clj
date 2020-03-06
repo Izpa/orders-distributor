136ed4cbdf7a7206efb4d3c05ee18a03febeec5c
Izpa <ispius.prime@gmail.com>
2020-03-06 03:53:36 +0300

    [WIP] Hydration

--- src/orders_distributor/models.clj
+++ src/orders_distributor/models.clj
@@ -1,6 +1,12 @@
 (ns orders-distributor.models
   (:require [toucan.models :as t]))
 
-(t/defmodel TelegramUser :telegram_user)
-(t/defmodel TelegramChat :telegram_chat)
+(t/defmodel TelegramUser :telegram_user
+  t/IModel
+  (hydration-keys [_]
+    [:telegram_user_id]))
+(t/defmodel TelegramChat :telegram_chat
+  t/IModel
+  (hydration-keys [_]
+    [:telegram_chat_id]))
 (t/defmodel TelegramMessage :telegram_message)
